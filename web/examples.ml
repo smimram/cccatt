6f4ad5d870dd633f3bce6542789cee8a77f78f9a
Samuel Mimram <smimram@gmail.com>
2024-11-29 15:27:11 +0100

    Birds.

--- web/examples.ml
+++ web/examples.ml
@@ -2,10 +2,12 @@ let tests = [%blob "../tests/test.cccatt"]
 let implicit = [%blob "../tests/implicit.cccatt"]
 let curry = [%blob "../examples/curry.cccatt"]
 let curien = [%blob "../examples/curien.cccatt"]
+let birds = [%blob "../examples/birds.cccatt"]
 
 let get = function
   | "tests" -> tests
   | "implicit" -> implicit
   | "curry" -> curry
   | "curien" -> curien
+  | "birds" -> curien
   | _ -> ""
