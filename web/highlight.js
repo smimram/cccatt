a9af57132832f64e28068cd8bbfcde9bd545f0fe
Samuel Mimram <smimram@gmail.com>
2024-12-02 21:35:52 +0100

    Split.

---
+++ web/highlight.js
@@ -0,0 +1,25 @@
+(async ({chrome, netscape}) => {
+  if (!chrome && !netscape) await import('https://unpkg.com/@ungap/custom-elements');
+  const {default: HighlightedCode} = await import('https://unpkg.com/highlighted-code');
+  const hljs = HighlightedCode.library;
+  hljs.registerLanguage('cccatt', function() {
+    return {
+      case_insensitive: false,
+      keywords: {
+        $pattern: /[->a-z:*=×→]+/,
+        keyword: ['coh','=',':='],
+        operator: ['->','→','*','×',':']
+      },
+      contains: [
+        {
+          className: 'string',
+          begin: '"',
+          end: '"'
+        },
+        hljs.COMMENT('#', '$', {})
+      ]
+    }
+  })
+  // https://github.com/highlightjs/highlight.js/tree/main/src/styles
+  HighlightedCode.useTheme('github');
+})(self);
