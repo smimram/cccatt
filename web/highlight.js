7a51b698b80f10bf9ef3f6b9744ca342248e25b7
Samuel Mimram <smimram@gmail.com>
2024-12-03 10:17:52 +0100

    More.

--- web/highlight.js
+++ web/highlight.js
@@ -6,9 +6,9 @@
     return {
       case_insensitive: false,
       keywords: {
-        $pattern: /[->a-z:*=×→]+/,
+        $pattern: /[->a-z:*=×→.]+/,
         keyword: ['coh','=',':='],
-        operator: ['->','→','*','×',':']
+        operator: ['->','→','*','×',':','.']
       },
       contains: [
         {
