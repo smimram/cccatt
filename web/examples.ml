58c4bb840a13b800331fa570edb1a07938b6a67a
Samuel Mimram <smimram@gmail.com>
2025-02-05 15:04:31 +0100

    Cartesian closed categories!

--- web/examples.ml
+++ web/examples.ml
@@ -1,5 +1,6 @@
 let tests = [%blob "../tests/test.cccatt"]
 let implicit = [%blob "../tests/implicit.cccatt"]
+let ccc = [%blob "../examples/ccc.cccatt"]
 let curry = [%blob "../examples/curry.cccatt"]
 let curien = [%blob "../examples/curien.cccatt"]
 let birds = [%blob "../examples/birds.cccatt"]
@@ -12,6 +13,7 @@ let get = function
   | "none" -> ""
   | "tests" -> tests
   | "implicit" -> implicit
+  | "ccc" -> ccc
   | "curry" -> curry
   | "curien" -> curien
   | "birds" -> birds
