c40a9a319b5454efcae41dc07a84b65e06a68d01
Samuel Mimram <smimram@gmail.com>
2025-02-28 21:06:45 +0100

    Zig-zag.

--- src/lang.ml
+++ src/lang.ml
@@ -22,6 +22,7 @@ let rec unify tenv env ?(alpha=[]) t t' =
   | Hom (a, b), Hom (a', b') -> unify tenv env a a'; unify tenv env b b'
   | Prod (a, b), Prod (a', b') -> unify tenv env a a'; unify tenv env b b'
   | One, One -> ()
+  | Op a, Op a' -> unify tenv env a a'
   | Pi (i, x, a, b), Pi (i', x', a', b') ->
     if i <> i' then raise Unification;
     unify tenv env a a';
